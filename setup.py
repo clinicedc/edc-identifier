b9720064870ef6517a73ce5b1496ccd3ec27f007
erikvw <ew2789@gmail.com>
2018-08-07 16:23:38 -0500

    setup, install_requires, requirements, PEP8

--- setup.py
+++ setup.py
@@ -25,6 +25,10 @@ setup(
     long_description=README,
     zip_safe=False,
     keywords='django base classes for identifiers',
+    install_requires=[
+        'edc-base',
+        'edc-model-admin',
+        'edc-registration'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
