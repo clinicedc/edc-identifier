e93fb95b41226172ce9d8908ee4d45ad6a15a2ee
erikvw <ew2789@gmail.com>
2019-04-04 15:06:43 -0500

    update mixin name

--- edc_identifier/views.py
+++ edc_identifier/views.py
@@ -2,11 +2,10 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
+from edc_dashboard.view_mixins import EdcViewMixin
 
-from edc_base.view_mixins import EdcBaseViewMixin
 
-
-class HomeView(EdcBaseViewMixin, TemplateView):
+class HomeView(EdcViewMixin, TemplateView):
 
     template_name = f"edc_identifier/bootstrap{settings.EDC_BOOTSTRAP}/home.html"
 
