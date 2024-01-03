79f580858df5b176422bdf8684cd14e1398fb96d
erikvw <ew2789@gmail.com>
2024-01-03 08:35:55 -0600

    update path for SiteModelMixin

--- edc_identifier/models.py
+++ edc_identifier/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db.models import UniqueConstraint
 from edc_model.models import BaseUuidModel
-from edc_sites.models import SiteModelMixin
+from edc_sites.model_mixins import SiteModelMixin
 
 
 class IdentifierModelManager(models.Manager):
